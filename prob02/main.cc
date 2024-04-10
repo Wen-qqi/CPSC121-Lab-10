f2c3d7e571607f1aa2c4270f470f781299f9eb1d
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-09 20:59:20 -0700

    first commit, finished code, passed all test, added header

--- prob02/main.cc
+++ prob02/main.cc
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 15, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 10-2
 // If it is a pair programming lab please specify partner below.
@@ -19,6 +18,7 @@ int main() {
   // =================== YOUR CODE HERE ===================
   // 1. Create a Bank object, name it anything you'd like :)
   // =======================================================
+  Bank new_bank("MyBank");
 
   // =================== YOUR CODE HERE ===================
   // 2. Create 3 new accounts in your bank.
@@ -29,13 +29,18 @@ int main() {
   //    * The 3nd account should belong to "Oreo", with
   //      a balance of $140.12
   // =======================================================
+  int account1 = new_bank.CreateAccount("Tuffy", 121.00);
+  new_bank.CreateAccount("Frank", 1234.56);
+  new_bank.CreateAccount("Oreo", 140.12);
 
   // =================== YOUR CODE HERE ===================
   // 3. Deactivate Tuffy's account.
   // =======================================================
+  new_bank.DeactivateAccount(account1);
 
   // =================== YOUR CODE HERE ===================
   // 4. Call DisplayBalances to print out all *active*
   //    account balances.
   // =======================================================
+  new_bank.DisplayBalances();
 }
