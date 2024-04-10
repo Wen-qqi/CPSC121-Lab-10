ae65cd017912f5cdee4e7adcda9ce077b55e1d16
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-09 20:58:47 -0700

    first commit, finshed implmenting bank class, added header

--- prob02/bank.h
+++ prob02/bank.h
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 15, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 10-2
 // If it is a pair programming lab please specify partner below.
@@ -18,6 +17,9 @@
 
 #include "account.h"
 
+#ifndef BANK_H
+#define BANK_H
+
 class Bank {
  public:
   // ======================= YOUR CODE HERE =======================
@@ -29,11 +31,49 @@ class Bank {
   // Pass objects by const reference when appropriate.
   // Remember that std::string is an object!
   // ===============================================================
+  Bank(const std::string& bank_name) : bank_name_(bank_name) {}
+
+  const std::string& GetBankName() const {
+    return bank_name_;
+  }
+
+  const std::map<int, Account>& GetAccounts() const {
+    return accounts_;
+  }
+
+  int CreateAccount(const std::string& bank_name, double initial_balance) {
+    Account new_account(bank_name, initial_balance);
+    int account_id = GenerateAccountId();
+    accounts_.insert({account_id, new_account});
+    return account_id;
+  }
+
+  int TotalAccounts() const {
+    return accounts_.size();
+  }
+
+  void DisplayBalances() {
+    for (std::map<int, Account>::iterator account_it = accounts_.begin();
+         account_it != accounts_.end(); account_it++) {
+      std::cout << account_it->second.GetAccountHolder() << ": "
+                << "$" << account_it->second.GetBalance() << "\n";
+    }
+  }
+
+  void DeactivateAccount(int id) {
+    std::map<int, Account>::iterator find_it = accounts_.find(id);
+    if (find_it != accounts_.end()) {
+      accounts_.erase(find_it);
+    }
+  }
 
  private:
   // We provided this helper function to you to randomly generate
   // a new Bank Account ID to be used in CreateAccount.
+  std::string bank_name_;
+  std::map<int, Account> accounts_;
   int GenerateAccountId() const {
     return std::rand() % 9000 + 1000;  // [1000, 9999]
   }
 };
+#endif
